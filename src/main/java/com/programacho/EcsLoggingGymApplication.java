bf81ce886e585cdcdbc7b89ec5eac1a6445074ee
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-02-06 23:08:44 +0900

    MDCの値がECS出力に反映されることを確認した。

--- src/main/java/com/programacho/EcsLoggingGymApplication.java
+++ src/main/java/com/programacho/EcsLoggingGymApplication.java
@@ -2,14 +2,14 @@ package com.programacho;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class EcsLoggingGymApplication {
 
     public static void main(String[] args) {
         Logger log = LoggerFactory.getLogger(EcsLoggingGymApplication.class);
+        MDC.put("mdc.key", "mdc.value");
 
         log.info("Hello World!");
-        log.warn("WARN");
-        log.error("ERROR");
     }
 }
